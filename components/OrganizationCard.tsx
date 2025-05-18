a7f58c5976c413fb8376b64e2d8627844a502683
Ivan <ijabadillalo@gmail.com>
2025-05-18 21:45:15 +0800

    feat: Create a responsive and reusable organization card component

---
+++ components/OrganizationCard.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+interface OrganizationCardProps {
+    borderColor: string;
+    organizationImage: string;
+    organizationName: string;
+    numberOfEvents: number;
+}
+
+export default function OrganizationCard({ 
+    borderColor, 
+    organizationImage, 
+    organizationName, 
+    numberOfEvents 
+}: OrganizationCardProps) {
+    return (
+        <div className={`flex items-center space-x-4 bg-white rounded-lg shadow px-4 py-3 border-l-[1rem] lg:border-l-[1.5rem] 2xl:border-l-[2rem] ${borderColor}`}>
+            <div className="w-8 h-8 xl:w-10 xl:h-10 2xl:w-16 2xl:h-16 relative flex-shrink-0">
+                <Image 
+                    src={organizationImage}
+                    alt={organizationName}
+                    fill
+                    className="rounded-full object-cover"
+                />
+            </div>
+            <div>
+                <p className="font-medium text-sm lg:text-base xl:text-lg 2xl:text-xl">{organizationName}</p>
+                <p className="text-gray-500 text-xs lg:text-sm xl:text-base 2xl:text-lg">{numberOfEvents} Events</p>
+            </div>
+        </div>
+    )
+};
